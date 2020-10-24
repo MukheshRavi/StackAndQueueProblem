365ab69c8a01151813fc1158db05ee143d6e4363
MukheshRavi <mukeshparvathi@gmail.com>
2020-10-24 23:25:06 +0530

    [Mukhesh].Add  Enqueue element in Queue

--- Program.cs
+++ Program.cs
@@ -7,15 +7,13 @@ namespace StackQueueDemo
         static void Main(string[] args)
         {
             ///creating object for stack class
-            StackClass<int> stackClass = new StackClass<int>();
-            stackClass.Push(70);
-            stackClass.Push(30);
-            stackClass.Push(56);
-            ///Displaying stack
-            stackClass.Display();
-            ///Pops all elements
-            stackClass.PopAll();
-            stackClass.Display();
+            QueueClass<int> queueClass = new QueueClass<int>();
+            queueClass.Enqueue(56);
+            queueClass.Enqueue(30);
+            queueClass.Enqueue(70);
+            ///Displaying Queue elements
+            queueClass.Display();
+           
            
         }
     }
